b53285edf191678bcdee7bd9ac85c28d3ab66b03
ZenX <ZenX@ZenXs-MacBook-Pro.local>
2017-07-18 23:26:22 +0200

    Added licence and Story model

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Story(models.Model):
+
+    title       = models.CharField('story title',max_length=255)
+    story_text  = models.TextField(default='')
+    pub_date    = models.DateTimeField(auto_now=True)
+    last_edited = models.DateTimeField(auto_now_add=True)
+    tags        = models.TextField(default='')
+    seen        = models.IntegerField(default=0)
+    votes       = models.IntegerField(default=0)
+    featured    = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name_plural = 'stories'
+        ordering = ['-pub_date', 'title']
